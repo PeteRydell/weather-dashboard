0e4008cd82c97dcb6bddc84b21e13219839c2f13
Peter Rydell <peterydell@gmail.com>
2019-10-22 21:09:06 -0700

    css

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ $("#find-city").on("click", function(event) {
     var cityIn = $("#city-input").val();
     console.log(cityIn)
 
-    //var queryURL = "https://openweathermap.org/data/2.5/weather?q=" + city + "&appid=b6907d289e10d714a6e88b30761fae22";
-
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityIn + "&appid=cd2c130356fc25320f54d47091844262";
     $.ajax({
         url: queryURL,

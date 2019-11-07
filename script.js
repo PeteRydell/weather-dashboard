e78eb29d8cd879956472ff40761dee7e53920458
Peter Rydell <peterydell@gmail.com>
2019-11-06 19:20:29 -0800

    five day ids

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-
 var cities = [];
+var fiveDay = [];
 
 $("#find-city").on("click", function(event) {
     event.preventDefault();
@@ -18,6 +17,7 @@ $("#find-city").on("click", function(event) {
 
 });
 
+//Renders current city wetaher
 function renderCitySearch() {
 
     $("#city-list").empty();
@@ -42,3 +42,26 @@ function renderCitySearch() {
 
 
 //var iconurl="http://openweathermap.org/img/wn/" + iconcode + ".png";
+
+// Renders 5 Day Forecast
+// function renderFiveDay() {
+
+//   $("#city-list").empty();
+
+//   for (var i = 0; i < cities.length; i++) {
+//       var cityBtn = ("<p>");
+//       cityBtn.addClass("city")
+//       cityBtn.attr("data-name", cities[i]);
+//       cityBtn.text(cities[i]);
+//       $("#city-list").append(cityBtn);
+//     }
+// }
+
+// $('#find-city').on('click', function(event) {
+//   event.preventDefault();
+//   var city = $('#city-list').val().trim();
+//   cities.push(city);
+//   renderCitySearch();
+// });
+
+// renderCitySearch();

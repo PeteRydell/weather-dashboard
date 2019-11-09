6b6aa680bce0cbd7029e16cdf36af2db59846485
Peter Rydell <peterydell@gmail.com>
2019-11-08 20:14:26 -0800

    storing and displaying city weather details

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var cities = [];
 
 $("#find-city").on("click", function(event) {
@@ -7,29 +5,61 @@ $("#find-city").on("click", function(event) {
 
     var cityIn = $("#city-input").val();
     console.log(cityIn)
-
+    var location = $(this).attr("data-name");
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityIn + "&appid=cd2c130356fc25320f54d47091844262";
+    
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        $("#city-weather").text(JSON.stringify(response));
-      });
 
-});
+          var cityDiv = $("<div id='city-weather'>");
+
+          // Storing the city name
+          var cityName = response.name;
+
+          // Creating an element to have the city name displayed
+          var pOne = $("<p>").text(cityName);
+
+          // Displaying the city
+          cityDiv.append(pOne);
+
+          // Storing the city temp
+          var cityTemp = response.temp;
+
+          // Creating an element to have the city temp displayed
+          var pTwo = $("<p>").text("Temperature: " + cityTemp);
+
+          // Displaying the temp
+          cityDiv.append(pTwo);
+
+          // Storing the city humidity
+          var cityHum = response.humidity;
+
+          // Creating an element to have the city humidity displayed
+          var pThree = $("<p>").text("Humidity: " + cityHum + "%");
+
+          // Displaying the humidity
+          cityDiv.append(pThree);
+
+          // Storing the city wind speed
+          var cityWind = response.humidity;
+
+          // Creating an element to have the city wind speed displayed
+          var pFour = $("<p>").text("Wind Speed: " + cityWind + " MPH");
+
+          // Displaying the wind speed
+          cityDiv.append(pFour);
+
+          // Putting the entire city info in the div
+          $("#city-weather").prepend(cityDiv);
+        });
+
+      });
 
 function renderCitySearch() {
 
-    $("#city-list").empty();
-
-    for (var i = 0; i < cities.length; i++) {
-        var cityBtn = ("<p>");
-        cityBtn.addClass("city")
-        cityBtn.attr("data-name", cities[i]);
-        cityBtn.text(cities[i]);
-        $("#city-list").append(cityBtn);
-      }
-  }
+  $("#city-list").empty();
 
   $('#find-city').on('click', function(event) {
     event.preventDefault();
@@ -39,6 +69,6 @@ function renderCitySearch() {
   });
 
   renderCitySearch();
-
+}
 
 //var iconurl="http://openweathermap.org/img/wn/" + iconcode + ".png";
